fce830cb50202f104a6cb7263f83ce328f990beb
jocelin79 <jocelinnunes@gmail.com>
2022-09-13 22:28:47 -0300

    up

--- frontend/main.js
+++ frontend/main.js
@@ -36,7 +36,7 @@ window.addEventListener('load', () => {
         task[5] = inputStatus.value;
         task[6] = count;
 
-        if(!task) {
+        if(!task[5]) {
             alert("Please fill out the task");
             return;
         }
@@ -97,6 +97,105 @@ window.addEventListener('load', () => {
 
         iteration();
 
+        inputName.value = "";
+        inputDescription.value = "";
+        inputFinalDate.value = "";
+        inputPriority.value = "";
+        inputCategory.value = "";
+        inputStatus.value = "";
+    });
+
+    edit_button.addEventListener('click', (e) => {
+        let task = [];
+
+        task[0] = inputName.value;
+        task[1] = inputDescription.value;
+        task[2] = inputFinalDate.value;
+        task[3] = inputPriority.value;
+        task[4] = inputCategory.value;
+        task[5] = inputStatus.value;
+
+        if(!task[5]) {
+            alert("Please fill out the task");
+            return;
+        }
+
+        if(!inputId.value) {
+            alert("Preencher Número da tarefa a ser Editada");
+            return;
+        }
+
+        taskList.forEach(e => {
+            if (e[6] == inputId) {
+                e[0] = inputName.value;
+                e[1] = inputDescription.value;
+                e[2] = inputFinalDate.value;
+                e[3] = inputPriority.value;
+                e[4] = inputCategory.value;
+                e[5] = inputStatus.value;
+            }
+        });
+
+        let child = list_el.lastElementChild;
+        while (child) {
+            list_el.removeChild(child);
+            child = list_el.lastElementChild;
+        };
+
+        let iteration = () => {
+
+            for (let i = 0; i < taskList.length; i++) {
+    
+                const task_el = document.createElement("div");
+                task_el.classList.add("task");
+        
+                const task_content_el = document.createElement("div");
+                task_content_el.classList.add("content");
+                task_content_el.innerText = ("Número: " + taskList[i][6] + "\n Tarefa: " + taskList[i][0] + "\n Descrição: " + taskList[i][1] + "\n Data Final: " + taskList[i][2] + "\n Prioridade: " + taskList[i][3] + "\n Categoria: " + taskList[i][4] + "\n Status: " + taskList[i][5]);
+        
+                task_el.appendChild(task_content_el);
+        
+                const task_actions_el = document.createElement("div");
+                task_actions_el.classList.add("actions");
+        
+                const task_delete_el = document.createElement("button");
+                task_delete_el.classList.add("delete");
+                task_delete_el.id = taskList[i][6];
+                task_delete_el.innerHTML = "Delete";
+        
+                task_actions_el.appendChild(task_delete_el);
+                
+                task_el.appendChild(task_actions_el);
+        
+                list_el.appendChild(task_el);
+    
+                task_delete_el.addEventListener('click', (a) => {
+
+                    let t = a.target;
+
+                    let taskId = t.id - 1
+
+                    taskList.splice(taskId, 1);
+
+                    let child = list_el.lastElementChild;
+                    while (child) {
+                        list_el.removeChild(child);
+                        child = list_el.lastElementChild;
+                    };
+
+                    taskList.forEach(b => {
+                        if (b[6] > t.id) b[6]--  
+                    });
+
+                    count--;
+
+                    iteration();
+                });
+            };
+        };
+
+
+        iteration();
 
         inputName.value = "";
         inputDescription.value = "";
@@ -104,19 +203,7 @@ window.addEventListener('load', () => {
         inputPriority.value = "";
         inputCategory.value = "";
         inputStatus.value = "";
+        inputId.value = "";
 
-        /*task_edit_el.addEventListener('click', () => {
-            if(task_edit_el.innerText.toLowerCase() == "edit") {
-                task_input_el.removeAttribute("readonly");
-                task_input_el.focus();
-                task_edit_el.inner = "Save";
-            } else {
-                task_input_el.setAttribute("readonly", "readonly");
-                task_edit_el,innerText = "Edit";
-            }
-        });*/
     });
-
-    edit_button.addEventListener('click', () => {
-    })
 })
